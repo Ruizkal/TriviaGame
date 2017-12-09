6c8f565a153c42c70271ad64931e1d0f0081c5ae
Kalvin Ruiz <edwardruiz@Kalvins-MacBook-Air.local>
2017-12-09 00:01:51 -0600

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,134 @@
+var questions = [{ 
+	"question": "What is Naruto's last name?" ,
+	"option 1": "Viz",
+	"option 2": "Goku",
+	"option 3": "Uzumaki",
+    "option 4": "weebTrash",
+    "answer": "3"
+}, {
+
+"question": "What is Sasukes's last name?" ,
+	"option 1": "ino",
+	"option 2": "Ben10",
+	"option 3": "weebTrash",
+    "option 4": "uchiha",
+    "answer": "4"
+}, {
+"question": "who is Itachi" ,
+	"option 1": "A hero",
+	"option 2": "Some Tv brand",
+	"option 3": "evil brother to sasuke",
+    "option 4": "clan killer",
+    "answer": "1"
+}, {
+"question": "Naruto has what tailed beast inside of him" ,
+	"option 1": "The one tail",
+	"option 2": "The Nine tails",
+	"option 3": "the ten tails",
+    "option 4": "a burrito",
+    "answer": "1"
+}, {
+"question": "Sasuke is known as Naruto's" ,
+	"option 1": "Love",
+	"option 2": "brother",
+	"option 3": "rival",
+    "option 4": "Father",
+    "answer": "3"
+},
+
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
